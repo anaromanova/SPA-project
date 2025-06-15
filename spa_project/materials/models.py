c0608ed7e0187b788071b7f528eab7f5fc0b70ce
AnastasiaR <anastasia.romanova3169@gmail.com>
2025-06-15 22:31:44 +0300

    new files

---
+++ spa_project/materials/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    preview = models.ImageField(upload_to='course_previews/', blank=True, null=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, related_name='lessons', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    preview = models.ImageField(upload_to='lesson_previews/', blank=True, null=True)
+    video_url = models.URLField()
+
+    def __str__(self):
+        return f"{self.title} ({self.course.title})"
+
